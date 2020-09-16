ba8c706c5e67f74014d732035cedeb16c0cbac1b
waleedElgazar <walidreda428@gmail.com>
2020-09-16 14:51:50 +0200

    models had been created

--- src/Moedls/teachStaff.java
+++ src/Moedls/teachStaff.java
@@ -8,4 +8,83 @@ public class teachStaff {
     private String jopTitle;
     private String gender;
     private String email;
+
+    public teachStaff(int id, String name, String nationalId, String phone, String jopTitle, String gender, String email) {
+        this.id = id;
+        this.name = name;
+        this.nationalId = nationalId;
+        this.phone = phone;
+        this.jopTitle = jopTitle;
+        this.gender = gender;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getJopTitle() {
+        return jopTitle;
+    }
+
+    public void setJopTitle(String jopTitle) {
+        this.jopTitle = jopTitle;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "teachStaff{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", nationalId='" + nationalId + '\'' +
+                ", phone='" + phone + '\'' +
+                ", jopTitle='" + jopTitle + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
