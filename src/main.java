83c864703f777cf641aa743c5e476a711f57d234
waleedElgazar <walidreda428@gmail.com>
2020-10-23 15:27:28 +0200

    last change with thread

--- src/main.java
+++ src/main.java
@@ -10,27 +10,35 @@ import java.util.List;
 public class main {
 
     public static void main(String[] args) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        //insert();
-        LocalTime time=LocalTime.now().plusMinutes(11);
-        String x=String.valueOf(formatter.format(time));
-        System.out.println(x);
-        Runnable task=new task("13:59");
-        Thread thread=new Thread(task);
-        thread.start();
+        insert();
+       while (true){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+            LocalTime time=LocalTime.now().plusMinutes(1);
+            String x=String.valueOf(formatter.format(time));
+            //System.out.println(x);
+            Runnable task=new task(x);
+            Thread thread=new Thread(task);
+            thread.start();
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void insert(){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         java.util.Date date=new java.util.Date();
         Date sqlDate=new Date(date.getTime());
-        for (int i = 10; i < 20; i++) {
-            LocalTime time=LocalTime.now().plusMinutes(3+i);
+        for (int i = 0; i < 20; i++) {
+            LocalTime time=LocalTime.now().plusMinutes(1+i);
             String x=String.valueOf(formatter.format(time));
             threadTask  task=new threadTask();
             task.inserttasks(new birth(i,"name",  sqlDate,x));
         }
     }
+
     static class task implements Runnable{
         Connection connection= DBConnection.getConnection();
         PreparedStatement pre;
@@ -47,7 +55,7 @@ public class main {
                 ResultSet resultSet;
                 resultSet=pre.executeQuery();
                 while (resultSet.next()){
-                    System.out.println(resultSet.getDate("date"));
+                    System.out.println(resultSet.getInt("id")+" "+resultSet.getDate("date")+" "+resultSet.getString("time"));
                 }
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
