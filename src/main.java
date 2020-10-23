3d830c908435fa6bef405ac9f3b2f096f060e61f
waleedElgazar <walidreda428@gmail.com>
2020-10-23 02:18:41 +0200

    update task

--- src/main.java
+++ src/main.java
@@ -1,43 +1,56 @@
+import Dao.DBConnection;
 import Models.birth;
 
-import java.sql.Timestamp;
-import java.sql.Date;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class main {
 
     public static void main(String[] args) {
-        Runnable task=new task();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        //insert();
+        LocalTime time=LocalTime.now().plusMinutes(11);
+        String x=String.valueOf(formatter.format(time));
+        System.out.println(x);
+        Runnable task=new task("13:59");
         Thread thread=new Thread(task);
-       // insert();
         thread.start();
     }
 
     public static void insert(){
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         java.util.Date date=new java.util.Date();
         Date sqlDate=new Date(date.getTime());
-        for (int i = 0; i < 10; i++) {
+        for (int i = 10; i < 20; i++) {
             LocalTime time=LocalTime.now().plusMinutes(3+i);
-            String x=String.valueOf(time);
+            String x=String.valueOf(formatter.format(time));
             threadTask  task=new threadTask();
             task.inserttasks(new birth(i,"name",  sqlDate,x));
         }
     }
     static class task implements Runnable{
-        threadTask  tasks=new threadTask();
-        List<birth> births=tasks.getAll();
-        java.util.Date date=new java.util.Date();
-        Timestamp sqlTime=new Timestamp(date.getTime());
-        String current=String.valueOf(sqlTime);
+        Connection connection= DBConnection.getConnection();
+        PreparedStatement pre;
+        String birthday;
+
+        public task(String birthday){
+            this.birthday=birthday;
+        }
         @Override
         public void run() {
-            for (int i = 0; i < births.size(); i++) {
-                if (current.equals(births.get(i).getTime())){
-                    System.out.println(births.get(i).getName());
+            try {
+                pre=connection.prepareStatement("SELECT * FROM birth WHERE birth.time = ?");
+                pre.setString(1,birthday);
+                ResultSet resultSet;
+                resultSet=pre.executeQuery();
+                while (resultSet.next()){
+                    System.out.println(resultSet.getDate("date"));
                 }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
             }
         }
     }
--- src/threadTask.java
+++ src/threadTask.java
@@ -42,5 +42,4 @@ public class threadTask {
         }
     return list;
     }
-
 }
